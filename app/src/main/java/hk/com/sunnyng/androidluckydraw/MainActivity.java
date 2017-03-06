790f3abaa6507cf2e4bce1c02aed9ecd778c0b72
Sunny NG <ngsanluk@gmail.com>
2017-03-06 18:33:05 +0800

    Add the event handler logics

--- app/src/main/java/hk/com/sunnyng/androidluckydraw/MainActivity.java
+++ app/src/main/java/hk/com/sunnyng/androidluckydraw/MainActivity.java
@@ -2,6 +2,10 @@ package hk.com.sunnyng.androidluckydraw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void drawNextLuckyNumber(View v) {
+        Random random = new Random();
+        int luckcyNumber = random.nextInt(100) + 1;
+        TextView luckyNumberTextView = (TextView) findViewById(R.id.lucky_number);
+        luckyNumberTextView.setText(String.valueOf(luckcyNumber));
+
     }
 }
